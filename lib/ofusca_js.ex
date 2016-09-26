c29d1c625a77ebd121acb8d9ebdc5979225de872
Helder de Sousa <helderhenri@gmail.com>
2016-09-26 15:44:42 -0300

    novo

--- lib/ofusca_js.ex
+++ lib/ofusca_js.ex
@@ -1,2 +1,36 @@
 defmodule OfuscaJs do
+
+	def parser(text) do
+		no_spaces = text |> String.split("\n") |> analise_split |> Enum.join("")
+
+		IO.puts("Inspecionando #{inspect no_spaces}")
+		no_spaces
+
+	end
+
+	def analise_split(split) do
+		lista = for n <- split do
+					    case n do
+							"" -> ""
+							n -> 
+								count_line = n |> String.length
+								IO.puts("#{inspect count_line}")
+								
+								a = n |> String.replace("  ","")
+								a = case Regex.run(~r/\/\//, a, return: :index) do
+										nil -> a
+										encontrado -> 
+											IO.puts("Encontrado: #{inspect encontrado} | #{count_line}")
+											[e] = encontrado
+											enc = elem(e,0) 
+											res = String.slice(a, 0,enc)
+											IO.puts("Encontrado: #{inspect enc} #{inspect res}")
+											res
+									end
+								a 
+						end
+					
+				end
+		lista
+	end
 end
