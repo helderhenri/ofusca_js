d2b8dc7114e3818655e075434c2aa2d621f03303
Helder de Sousa <helderhenri@gmail.com>
2016-09-26 18:41:38 -0300

    removido IO.puts

--- lib/ofusca_js.ex
+++ lib/ofusca_js.ex
@@ -2,8 +2,6 @@ defmodule OfuscaJs do
 
 	def parser(text) do
 		no_spaces = text |> String.split("\n") |> analise_split |> Enum.join("")
-
-		IO.puts("Inspecionando #{inspect no_spaces}")
 		no_spaces
 
 	end
@@ -14,17 +12,14 @@ defmodule OfuscaJs do
 							"" -> ""
 							n -> 
 								count_line = n |> String.length
-								IO.puts("#{inspect count_line}")
 								
 								a = n |> String.replace("  ","")
 								a = case Regex.run(~r/\/\//, a, return: :index) do
 										nil -> a
 										encontrado -> 
-											IO.puts("Encontrado: #{inspect encontrado} | #{count_line}")
 											[e] = encontrado
 											enc = elem(e,0) 
 											res = String.slice(a, 0,enc)
-											IO.puts("Encontrado: #{inspect enc} #{inspect res}")
 											res
 									end
 								a 
